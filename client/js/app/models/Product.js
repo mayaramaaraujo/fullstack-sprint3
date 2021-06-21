0b12117ad5e0c9ba9b23215864e6f4155b7d5562
DESKTOP-JVIJUL5\55659 <mayaramaaraujo98@gmail.com>
2021-06-21 10:04:20 -0300

    #1 implementation of the 'product and product list' classes

--- client/js/app/models/Product.js
+++ client/js/app/models/Product.js
@@ -1,3 +1,22 @@
 class Product {
-  // TODO: implementar
+  
+  constructor(image, description, price) {
+    this._image = image;
+    this._description = description;
+    this._price = price;
+    Object.freeze(this);
+  }
+
+  get image() {
+    return this._image;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get price() {
+    return this._price;
+  }
+
 }
